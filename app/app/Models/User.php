1ad4b975f7bff3d33eff049f37eee87527e30f31
sixsad <miller@sputnikfund.ru>
2024-03-16 19:17:55 +0700

    update User table

--- app/app/Models/User.php
+++ app/app/Models/User.php
@@ -2,43 +2,73 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Hash;
 use Laravel\Sanctum\HasApiTokens;
+use Laravel\Sanctum\NewAccessToken;
 
+/**
+ * Table: users
+ *
+ * === Columns ===
+ * @property int $id
+ * @property string $email
+ * @property Carbon $created_at
+ * @property Carbon $updated_at
+ * === Relationships ===
+ * @property-read Cart|Collection $cart
+ * @property-read Order|Collection $orders
+ * @property-read Balance|Collection $balance
+ */
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasApiTokens;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
-        'name',
         'email',
-        'password',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+    protected static function boot(): void
+    {
+        parent::boot();
+        static::created(fn(self $user) => $user->createCart());
+        static::created(fn(self $user) => $user->createBalance());
+    }
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    protected function createCart(): void
+    {
+        $this->cart()->create();
+    }
+
+    protected function createBalance(): void
+    {
+        $this->balance()->create();
+    }
+
+    public function generateApiToken(): NewAccessToken
+    {
+        return $this->createToken(Hash::make($this->email));
+    }
+
+    public function cart(): HasOne
+    {
+        return $this->hasOne(Cart::class)->latest();
+    }
+
+    public function balance(): HasOne
+    {
+        return $this->hasOne(Balance::class);
+    }
+
+    public function orders(): HasMany
+    {
+        return $this->hasMany(Order::class);
+    }
+
 }
